2fe938bba74cabfbd6c96fff88bf244a279e8639
sentientmachin3 <davideb1912@gmail.com>
2025-06-25 10:35:12 +0200

    fix typos

--- app/data.ts
+++ app/data.ts
@@ -3,29 +3,29 @@ import { WorkExperience } from "../types"
 
 export const workExperiences: WorkExperience[] = [
     {
-        start: DateTime.fromObject({ month: 10, year: 2023 }),
+        start: DateTime.fromObject({ month: 11, year: 2023 }),
         end: "now",
-        workName: "Dee Jungle",
-        workplace: "Software Developer",
+        workplace: "Dee Jungle",
+        workName: "Full Stack Software Developer",
     },
     {
-        start: DateTime.fromObject({ month: 8, year: 2022 }),
-        end: DateTime.fromObject({ month: 10, year: 2023 }),
+        start: DateTime.fromObject({ month: 9, year: 2022 }),
+        end: DateTime.fromObject({ month: 11, year: 2023 }),
         workplace: "Bit Mobility",
-        workName: "Software Developer",
+        workName: "Full Stack Software Developer",
     },
     {
-        start: DateTime.fromObject({ month: 10, year: 2020 }),
-        end: DateTime.fromObject({ month: 7, year: 2022 }),
+        start: DateTime.fromObject({ month: 11, year: 2020 }),
+        end: DateTime.fromObject({ month: 8, year: 2022 }),
         workplace: "Iotinga",
-        workName: "Software Developer",
+        workName: "Full Stack Software Developer",
     },
 ]
 
 export const skills: string[] = [
     "Backend development via GraphQL or REST frameworks like ExpressJS",
     "Relational databases (Postgres, MySQL, MariaDB) and non-relational databases (AWS DynamoDB, CouchDB, Redis)",
-    "IoT devices integrations via MQTT",
+    "IoT devices integrations",
     "Integrations with modern payment systems (Stripe, Satispay, Paypal)",
     "Front-end web development, in ReactJS, Flask and Angular",
     "Mobile cross-platform applications with React Native",
